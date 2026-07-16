16d360fd4026aca43b661ab6f26384d3ba60c7bb
ZhengHe <15166143379@163.com>
2026-07-16 20:14:37 +0800

    增加文件名校验

--- agent_lab/file_tool.go
+++ agent_lab/file_tool.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"unicode/utf8"
+	"regexp"
 	"strings"
 )
 
@@ -22,7 +23,7 @@ func (r *RuneTooLongError) Error()string {
 	return fmt.Sprintf("内容过长, 限制 %d 字符, 当前 %d 字符", r.limit, r.actual)
 }
 
-func validateContent(content string)error { // 内容检验
+func validateContent(content string)error { // Content内容校验
 
 	if content = strings.TrimSpace(content); content == "" { // 判断是否为空
 		return ErrContentEmpty
@@ -40,6 +41,16 @@ func validateContent(content string)error { // 内容检验
 			limit: 40000,
 			actual: size,
 		}
+	}
+
+	return nil
+}
+
+var filenameRule = regexp.MustCompile(`^[a-zA-Z0-9][a-zA-Z0-9.-_]{0,99}\.txt$`) // 编译可复用的规则
+
+func validateFilename(filename string)error { // 文件名校验
+	if ! filenameRule.MatchString(filename) { //用正则表达式规则判断 传入值是否合规
+		return ErrInvalidFilename
 	}
 
 	return nil
