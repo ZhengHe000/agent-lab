48ac989cd676cecdbdc2d6af5dc228554925f284
ZhengHe <15166143379@163.com>
2026-07-16 20:14:37 +0800

    增加文件名校验

--- agent_lab/errors.go
+++ agent_lab/errors.go
@@ -5,6 +5,9 @@ import (
 )
 
 var (
-	// 一个string类型 的内容为""(也就是空)时 应该使用的哨兵错误
+	// 一个string类型 的 内容为""(也就是空)时 应该使用的错误
 	ErrContentEmpty = errors.New("错误, 因为存在内容为空")
+
+	// 一个内容格式 与 设定的正则表达式规则 不符 应该使用的错误
+	ErrInvalidFilename = errors.New("错误, 因为存在与设定的正则表达式规则不符的内容")
 )
