37da7353c3b364d09bfc90b5eb9166052404b5a9
ZhengHe <15166143379@163.com>
2026-07-21 23:22:43 +0800

    增加测试,修复逻辑问题

--- agent_lab/internal/config/env.go
+++ agent_lab/internal/config/env.go
@@ -3,31 +3,52 @@ package config
 import (
 	"bufio"
 	"fmt"
+	"github.com/ZhengHe000/agent-lab/agent_lab/internal/workspace"
 	"os"
 	"strings"
 )
 
-func LoadEnv(fileName string) error { // 配置环境变量
+func LoadEnvFile(fileName string) error { // 配置环境变量
 	file, err := os.Open(fileName) //打开env环境配置文件
 	if err != nil {
-		return fmt.Errorf("打开 %v 文件失败, 错误: %w", fileName, err)
+		return fmt.Errorf("%w:%w", workspace.ErrReadFile, err)
 	}
-
 	defer file.Close() // 结束前关闭文件
 
 	scanner := bufio.NewScanner(file) // 创建扫描器
+	lineNumber := 0                   // 用来计算当前第几行
 
 	for scanner.Scan() { // 逐行扫描
+		lineNumber++
 
 		line := strings.TrimSpace(scanner.Text()) // 拿到一行内容
-		parts := strings.SplitN(line, "=", 2)     // 以=分开
 
-		if len(parts) == 2 { // 有键值对便当作环境变量使用
-			os.Setenv(strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])) // 设置环境变量
+		if line == "" || strings.HasPrefix(line, "#") { // 跳过空行和注释
+			continue
 		}
+
+		parts := strings.SplitN(line, "=", 2) // 以=分开
+
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+
+		if key == "" { // 检查配置名称
+			return fmt.Errorf("第 %d 行的环境变量名称为空", lineNumber)
+		}
+
+		if _, exists := os.LookupEnv(key); exists { // 查看配置是否存在
+			continue
+		}
+
+		if err := os.Setenv(key, value); err != nil { // 进行环境配置
+			return fmt.Errorf("[环境配置] 第 %d 行失败:%w", lineNumber, err)
+		}
+
 	}
+
 	if err := scanner.Err(); err != nil {
-		return err
+		return fmt.Errorf("[环境配置|扫描器异常]:%w", err)
 	}
+
 	return nil
 }
