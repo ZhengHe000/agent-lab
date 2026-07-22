8334246aa56754638df999dd0178b214148c53ec
ZhengHe <15166143379@163.com>
2026-07-22 21:57:27 +0800

    修复错误理解的注释,优化结构体字段名

--- agent_lab/internal/model/model.go
+++ agent_lab/internal/model/model.go
@@ -1,42 +1,42 @@
 package model
 
 import (
-	"encoding/json"
 	"context"
+	"encoding/json"
 )
 
 type Role string
 
 const (
-	RoleSystem Role = "system" // 系统级
-	RoleUser Role = "user" // 用户级
+	RoleSystem    Role = "system"    // 系统级
+	RoleUser      Role = "user"      // 用户级
 	RoleAssistant Role = "assistant" // 模型级
-	RoleTool Role = "tool" // 工具级
+	RoleTool      Role = "tool"      // 工具级
 )
 
 type ToolCall struct { // 模型使用工具时需要生成的信息
-	ID string // 具体工具的调用顺序 例如:(查目录ID:1, 调浏览器ID:2, 读取链接文档ID:3)
-	Name string // 工具名
+	ID        string          // 模型为一个工具调用分配的标识, 在工具调用结果封装时原封不动返回给模型, 模型看到这个标识就知道当前这份数据来自什么
+	Name      string          // 工具名
 	Arguments json.RawMessage // 模型对具体工具按要求生成的操作参数, 使用json.RawMessage方便使用具体工具前 解析到当前工具的参数结构体
 }
 
 type Message struct { // 通用消息
-	Role Role // 身份
-	Content string // 正文, 不使用*string, 因为是否为null应该在上层做判断, 这里直接判断是否为""
-	ToolCalls []ToolCall // 单个或多工具信息
-	ToolCallID string // 工具函数返回时携带的自身信息, 告诉模型一段工具调用结果来自什么具体工具
+	Role       Role       // 身份
+	Content    string     // 正文, 不使用*string, 因为是否为null应该在上层做判断, 这里直接判断是否为""
+	ToolCalls  []ToolCall // 单个或多工具信息
+	ToolCallID string     // 工具结果所对应的模型调用ID, 装的是调用工具响应里模型为一次操作分配的唯一ID
 }
 
 type Request struct {
-	Model string // 模型名
-	Messages []Message 
+	Model    string // 模型名
+	Messages []Message
 }
 
 type Response struct {
-	Messages Message
+	Message      Message
 	FinishReason string // 模型结束循环的理由
 }
 
 type Model interface { // 定义Model接口
-	Complete(ctx context.Context, request Request)(Response, error) // 只要实现Complete函数作为方法, 就可以当作Model接口使用
+	Complete(ctx context.Context, request Request) (Response, error) // 只要实现Complete函数作为方法, 就可以当作Model接口使用
 }
