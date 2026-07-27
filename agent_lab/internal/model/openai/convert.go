31e7fcfe61cc651d60fe0b45e5c0f38c6aea730e
ZhengHe <15166143379@163.com>
2026-07-27 16:03:52 +0800

    增加model请求向外部请求的转换 和对应测试

--- agent_lab/internal/model/openai/convert.go
+++ agent_lab/internal/model/openai/convert.go
@@ -7,7 +7,7 @@ import (
 	"github.com/ZhengHe000/agent-lab/agent_lab/internal/model"
 )
 
-func toModelResponse(response chatCompletionResponse) (model.Response, error) {
+func toModelResponse(response chatCompletionResponse) (model.Response, error) { // 将外部响应转换成内部结构
 	if len(response.Choices) == 0 {
 		return model.Response{}, fmt.Errorf("模型响应Choices为空")
 	}
@@ -81,4 +81,68 @@ func toModelMessage(chatMessage chatMessage) (model.Message, error) {
 	modelMessage.ToolCalls = toolCalls // 将结果赋值给ToolCalls字段
 
 	return modelMessage, nil
+}
+
+func toChatCompletionRequest(request model.Request) (chatCompletionRequest, error) { // 将内部请求转换成外部请求
+	if len(request.Messages) == 0 {
+		return chatCompletionRequest{}, fmt.Errorf("内部请求信息的Messages为空, 无法转换为外部请求")
+	}
+	reqMessages := make([]chatMessage, 0, len(request.Messages)) // 创建变量容器
+
+	for _, modelReqMessage := range request.Messages { // 遍历内部信息切片, 每轮使用toChatMessage函数处理转换
+		chatMessage, err := toChatMessage(modelReqMessage)
+		if err != nil {
+			return chatCompletionRequest{}, err
+		}
+		reqMessages = append(reqMessages, chatMessage) // 转换成功追加进容器
+	}
+
+	chatCompletionRequest := chatCompletionRequest{ // 拼接目标字段
+		Model:    request.Model,
+		Messages: reqMessages,
+	}
+	return chatCompletionRequest, nil
+}
+
+func toChatMessage(modelMessage model.Message) (chatMessage, error) {
+	var content *string                                                 // 定义content字段的容器, 默认为nil
+	if modelMessage.Content != "" || len(modelMessage.ToolCalls) == 0 { // 当内部此字段有值 或 不存在工具调用(此时空值就是模型输出) 使用内部content的地址作为值
+		content = &modelMessage.Content
+	}
+
+	var role string            // 定义一个role容器
+	switch modelMessage.Role { //使用白名单得到role结果 或 退出错误
+	case model.RoleSystem:
+		role = "system"
+	case model.RoleUser:
+		role = "user"
+	case model.RoleAssistant:
+		role = "assistant"
+	case model.RoleTool:
+		role = "tool"
+	default:
+		return chatMessage{}, fmt.Errorf("Role类型不符合预设: %q", modelMessage.Role)
+	}
+
+	chatToolCalls := make([]chatToolCall, 0, len(modelMessage.ToolCalls)) // 创建chatToolCalls容器, 储存和追加从内部信息遍历出来进行转换后的chatToolCall值
+	for _, toolCall := range modelMessage.ToolCalls {
+		chatToolCall := chatToolCall{
+			ID:   toolCall.ID,
+			Type: "function", // 默认写死function
+			Function: chatFunctionCall{
+				Name:      toolCall.Name,
+				Arguments: string(toolCall.Arguments), //将内部信息强转换为string
+			},
+		}
+
+		chatToolCalls = append(chatToolCalls, chatToolCall) // 将一个转换结果追加进chatToolCalls容器
+	}
+	chatMessage := chatMessage{ // 组装chatMessage结构体
+		Role:       role,
+		Content:    content,
+		ToolCalls:  chatToolCalls,
+		ToolCallID: modelMessage.ToolCallID,
+	}
+
+	return chatMessage, nil
 }
